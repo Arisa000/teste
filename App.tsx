24536e9bdc3394f2bc418b9e1dc226fc59e1cdd3
Ana <ana.fuiguti@estudante.ifms.edu.br>
2021-08-31 23:34:36 -0300

    Ajustando campo de pesquisa

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={require("./src/assets/arrow_left.png")}></Image>
-        <Image source={require("./src/assets/search.png")}></Image>
+
+        <View style={styles.inputTextView}>
+          <Image source={require("./src/assets/search.png")}></Image>
+          <TextInput 
+            placeholder="Search" 
+            placeholderTextColor="#F5FFFF"
+            style={styles.textInput}>
+          </TextInput>
+        </View>
+        
         <Image source={require("./src/assets/share.png")}></Image>
       </View>
       <Text>Hello World</Text>
@@ -21,7 +30,24 @@ const styles = StyleSheet.create({
   },
   header: {
     minHeight: 93,
-    backgroundColor: '#2C4877',
+    backgroundColor: '#4369B0',
     paddingTop: 40,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+  },
+  inputTextView: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: '#2C4877',
+    height: 40,
+    marginHorizontal: 10,
+    alignItems: "center",
+    borderRadius: 28,
+  },
+  textInput: {
+    color: '#F5FFFF',
+    flex: 1,
+    paddingRight: 10,
   },
 });
