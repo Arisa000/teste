1dbf3ea587c827ba202eeb1c3327e6e96d7a7d60
Ana <ana.fuiguti@estudante.ifms.edu.br>
2021-08-31 22:53:39 -0300

    Incluindo imagens do projeto

--- App.tsx
+++ App.tsx
@@ -5,6 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Text></Text>
+      </View>
       <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,9 +16,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    height: 93,
+    backgroundColor: '#2C4877',
+    paddingTop: 40,
   },
 });
