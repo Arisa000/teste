ec7b882f1231748d475e4e65274305d1e63417d6
Ana <ana.fuiguti@estudante.ifms.edu.br>
2021-09-13 18:02:37 -0300

    Transferindo a APP.tsx para o componente Home

--- App.tsx
+++ App.tsx
@@ -1,93 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { Text, View } from 'react-native';
 
-const LinkItens = (props:any)=>{
-  return(
-    <Text style={stylesLink.text}>{props.name}</Text>
-  );
-}
-
-const stylesLink = StyleSheet.create({
-  text:{
-    marginTop: 10,
-    color: '#4A4A4C',
-    fontSize: 13.71,
-
-  }
-});
-
-const LinksImportantes = ()=>{
-  return(// O JSX ficará aqui
-    <View style={stylesLinksImportantes.container}>
-      <LinkItens name="Home"/>
-      <LinkItens name="Post"/>
-      <LinkItens name="Videos"/>
-      <LinkItens name="Photos"/>
-      <LinkItens name="Comunity"/>
-    </View>
-  );
-};
-
-const stylesLinksImportantes = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingHorizontal: 40,
-    paddingVertical: 10,
-
-    borderBottomColor: "red",
-    borderBottomWidth: 1,
-  }
-});
+import Home from './src/Pages/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Image source={require("./src/assets/arrow_left.png")}></Image>
-
-        <View style={styles.inputTextView}>
-          <Image source={require("./src/assets/search.png")}></Image>
-          <TextInput 
-            placeholder="Search" 
-            placeholderTextColor="#F5FFFF"
-            style={styles.textInput}>
-          </TextInput>
-        </View>
-        
-        <Image source={require("./src/assets/share.png")}></Image>
-      </View>
-      <LinksImportantes />
-      <StatusBar style="auto" />
-    </View>
+    <View><Home/></View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  header: {
-    minHeight: 93,
-    backgroundColor: '#4369B0',
-    paddingTop: 40,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingHorizontal: 30,
-  },
-  inputTextView: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: '#2C4877',
-    height: 40,
-    marginHorizontal: 10,
-    alignItems: "center",
-    borderRadius: 28,
-  },
-  textInput: {
-    color: '#F5FFFF',
-    flex: 1,
-    paddingRight: 10,
-  },
-});
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,12 +1,95 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
+const LinkItens = (props:any)=>{
+    return(
+      <Text style={stylesLink.text}>{props.name}</Text>
+    );
+  }
+  
+  const stylesLink = StyleSheet.create({
+    text:{
+      marginTop: 10,
+      color: '#4A4A4C',
+      fontSize: 13.71,
+  
+    }
+  });
+  
+  const LinksImportantes = ()=>{
+    return(// O JSX ficará aqui
+      <View style={stylesLinksImportantes.container}>
+        <LinkItens name="Home"/>
+        <LinkItens name="Post"/>
+        <LinkItens name="Videos"/>
+        <LinkItens name="Photos"/>
+        <LinkItens name="Comunity"/>
+      </View>
+    );
+  };
+  
+  const stylesLinksImportantes = StyleSheet.create({
+    container: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingHorizontal: 40,
+      paddingVertical: 10,
+  
+      borderBottomColor: "red",
+      borderBottomWidth: 1,
+    }
+  });
+  
 const Home = () => {
     return(
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("../../assets/arrow_left.png")}></Image>
+
+        <View style={styles.inputTextView}>
+          <Image source={require("../../assets/search.png")}></Image>
+          <TextInput 
+            placeholder="Search" 
+            placeholderTextColor="#F5FFFF"
+            style={styles.textInput}>
+          </TextInput>
         </View>
+        
+        <Image source={require("../../assets/share.png")}></Image>
+      </View>
+      <LinksImportantes />
+      <StatusBar style="auto" />
+    </View>
+    
     );
 }
-
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    header: {
+      minHeight: 93,
+      backgroundColor: '#4369B0',
+      paddingTop: 40,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingHorizontal: 30,
+    },
+    inputTextView: {
+      flex: 1,
+      flexDirection: "row",
+      backgroundColor: '#2C4877',
+      height: 40,
+      marginHorizontal: 10,
+      alignItems: "center",
+      borderRadius: 28,
+    },
+    textInput: {
+      color: '#F5FFFF',
+      flex: 1,
+      paddingRight: 10,
+    },
+  });
+  
 export default Home;
