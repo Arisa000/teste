593beb64694bbbb6603e70f0bbe03c2b4940c4f7
Ana <ana.fuiguti@estudante.ifms.edu.br>
2021-09-29 17:40:38 -0300

    Linkshar

--- src/Pages/Post/index.tsx
+++ src/Pages/Post/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, Text, View, Image, TextInput, StyleSheet } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 const Post = ()=>{
     return(
@@ -17,6 +17,49 @@ const Post = ()=>{
                         )}></Image>
                     </View>
                 </View>
+            </View>
+            <View style={styles.textPostPub}>
+                <Text style={styles.textPostPubText}>Este é o design que vamos replicar hoje na aula: 
+                    <Text style={styles.textPostPubLink}>https://mockupsfreebies.com/social-media/facebook/free-facebook-mobile-post-mockup</Text>
+                </Text>
+                <Image style={styles.imgPost}  
+                    source = {require('../../assets/post.png')}>
+                </Image>
+            </View>
+            <View style={styles.like}>
+                <Image style={styles.imgLikes} 
+                    source = {require(
+                        '../../assets/likes.png'
+                    )}>
+                </Image>
+                <Text style={styles.likeText}>12</Text>
+            </View>
+            <View style={styles.linkShare}>
+                <View style={styles.linksShareBloc}>
+                    <Image style={styles.linksShareBlocIcon}
+                        source = {require(
+                            '../../assets/like.png'
+                        )}>
+                    </Image>
+                    <Text>Likes</Text>
+                </View>
+                <View style={styles.linksShareBloc}>
+                    <Image style={styles.linksShareBlocIcon}
+                        source = {require(
+                            '../../assets/comentario.png'
+                        )}>
+                    </Image>
+                    <Text>Comentários</Text>
+                </View>
+                <View style={styles.linksShareBloc}>
+                    <Image style={styles.linksShareBlocIcon}
+                        source = {require(
+                            '../../assets/share1.png'
+                        )}>
+                    </Image>
+                    <Text>Compartilhar</Text>
+                </View>
+                
             </View>
         </View>
     );
@@ -50,5 +93,47 @@ const styles = StyleSheet.create({
     },
     img:{
         marginLeft: 6,
+    },
+    textPostPub:{
+        marginTop: 10,
+        fontSize: 18,
+    },
+    textPostPubLink:{
+        color: '#4D6196',
+        fontSize: 15,
+    },
+    textPostPubText:{
+        fontSize: 15,
+    },
+    imgPost:{
+        width: 358,
+        height: 282,
+    },
+    imgLikes:{
+        width: 30,
+        height: 30,
+    },
+    like:{
+        marginTop: 30,
+        flexDirection: 'row',
+        alignContent: 'center',
+    },
+    likeText:{
+        marginLeft: 10,
+        textAlignVertical: 'center',
+    },
+    linkShare:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginHorizontal: 20,
+        marginTop: 10,
+    },
+    linksShareBloc:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    linksShareBlocIcon:{
+        width: 18,
+        height: 18,
     },
 });
